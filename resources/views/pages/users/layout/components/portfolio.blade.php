0cea6560a7f53b773ecd9b9400d2cbe15c5de4c1
fifo augie <fifoaugie06@gmail.com>
2020-08-21 23:24:05 +0700

    Templating laravel

---
+++ resources/views/pages/users/layout/components/portfolio.blade.php
@@ -0,0 +1,91 @@
+<div class="container" data-aos="fade-up">
+
+    <header class="section-header">
+        <h3 class="section-title">Our Portfolio</h3>
+    </header>
+
+    <div class="row" data-aos="fade-up" data-aos-delay="100">
+        <div class=" col-lg-12">
+            <ul id="portfolio-flters">
+                <li data-filter="*" class="filter-active">All</li>
+                <li data-filter=".filter-app">App</li>
+                <li data-filter=".filter-card">Card</li>
+                <li data-filter=".filter-web">Web</li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="row portfolio-container" data-aos="fade-up" data-aos-delay="200">
+
+        <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+            <div class="portfolio-wrap">
+                <figure>
+                    <img src="{{asset('template/assets/img/portfolio/app1.jpg')}}" class="img-fluid" alt="">
+                    <a href="{{asset('template/assets/img/portfolio/app1.jpg')}}" data-lightbox="portfolio"
+                       data-title="App 1"
+                       class="link-preview"><i class="ion ion-eye"></i></a>
+                    <a href="portfolio-details.html" class="link-details" title="More Details"><i
+                            class="ion ion-android-open"></i></a>
+                </figure>
+
+                <div class="portfolio-info">
+                    <h4><a href="portfolio-details.html">App 1</a></h4>
+                    <p>App</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6 portfolio-item filter-web">
+            <div class="portfolio-wrap">
+                <figure>
+                    <img src="{{asset('template/assets/img/portfolio/web3.jpg')}}" class="img-fluid" alt="">
+                    <a href="{{asset('template/assets/img/portfolio/web3.jpg')}}" class="link-preview venobox"
+                       data-gall="portfolioGallery" title="Web 3"><i class="ion ion-eye"></i></a>
+                    <a href="portfolio-details.html" class="link-details" title="More Details"><i
+                            class="ion ion-android-open"></i></a>
+                </figure>
+
+                <div class="portfolio-info">
+                    <h4><a href="portfolio-details.html">Web 3</a></h4>
+                    <p>Web</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+            <div class="portfolio-wrap">
+                <figure>
+                    <img src="{{asset('template/assets/img/portfolio/app2.jpg')}}" class="img-fluid" alt="">
+                    <a href="{{asset('template/assets/img/portfolio/app2.jpg')}}" class="link-preview venobox"
+                       data-gall="portfolioGallery" title="App 2"><i class="ion ion-eye"></i></a>
+                    <a href="portfolio-details.html" class="link-details" title="More Details"><i
+                            class="ion ion-android-open"></i></a>
+                </figure>
+
+                <div class="portfolio-info">
+                    <h4><a href="portfolio-details.html">App 2</a></h4>
+                    <p>App</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-lg-4 col-md-6 portfolio-item filter-web">
+            <div class="portfolio-wrap">
+                <figure>
+                    <img src="{{asset('template/assets/img/portfolio/web1.jpg')}}" class="img-fluid" alt="">
+                    <a href="{{asset('template/assets/img/portfolio/web1.jpg')}}" class="link-preview venobox"
+                       data-gall="portfolioGallery" title="Web 1"><i class="ion ion-eye"></i></a>
+                    <a href="portfolio-details.html" class="link-details" title="More Details"><i
+                            class="ion ion-android-open"></i></a>
+                </figure>
+
+                <div class="portfolio-info">
+                    <h4><a href="portfolio-details.html">Web 1</a></h4>
+                    <p>Web</p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+</div>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('/')->namespace('Users')->group(function (){
+    Route::get('', 'LandingPageController@index');
 });
